b14b8fbad124a5847e36ddfb78cb8be92ad4b371
jfhaecker <joerghaecker@gmx.net>
2018-05-15 11:44:17 +0200

    huhu

---
+++ golang/fakultaet/fakultaet.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+import "os"
+import "strconv"
+
+func main() {
+	argsWithoutProg := os.Args[1:] 
+	if(len(argsWithoutProg) == 0) {
+		fmt.Println("Bitte um Zahl > 0. Bye")
+		os.Exit(2)
+	}
+	args := argsWithoutProg[0]  
+	n, err := strconv.Atoi(args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	} 
+	if n < 1 {
+		fmt.Println("Bitte um Zahl > 0. Bye")
+		os.Exit(2)
+	}
+	fmt.Println("Fak(",n,")=",fak(n))
+
+}
+
+func fak(n int) int {
+	if(n==1) {
+		return 1
+	} else { 
+		return n*fak(n-1)
+	}
+}
