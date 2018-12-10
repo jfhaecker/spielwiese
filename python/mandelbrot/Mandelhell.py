6c66c998967be569f25751fec62a42aed3447bca
jfhaecker <joerghaecker@gmx.net>
2018-12-10 22:16:22 +0100

    Hallo

--- python/mandelbrot/Mandelhell.py
+++ python/mandelbrot/Mandelhell.py
@@ -1,6 +1,7 @@
 import numpy as np 
 from numba import jit
 from matplotlib import pyplot as plt
+from matplotlib import colors 
 
 @jit
 def mandelbrot(c, maxiter):
@@ -22,13 +23,36 @@ def mandelbrot_set(xmin, xmax, ymin, ymax, width, height, maxiter):
     return(r1, r2, n3)
 
 
-def mandelbrot_image(xmin, xmax, ymin, ymax, width, height, maxiter):
-    x,y,z = mandelbrot_set(xmin, xmax, ymin, ymax, width, height, maxiter)
+def mandelbrot_image(xmin, xmax, ymin, ymax, width, height, maxiter, cmap='jet'):
+    dpi = 100
+    img_width = width * dpi
+    img_height = height * dpi
+    ticks = np.arange(0, img_width, 1*dpi)
+    print("Ticks {a}".format(a=ticks))
+    print("....> {a}".format(a=ticks/img_width))
+    print("Length:{a}".format(a=xmax-xmin))
+    x_ticks = xmin + (xmax-xmin)*ticks/img_width
+    print("XTicks {a}".format(a=x_ticks))
+    x,y,z = mandelbrot_set(xmin, xmax, ymin, ymax, img_width, img_height, maxiter)
     print("Hallo")
     print(z.T)
-    plt.imshow(z.T, origin='lower')
+    plt.figure(figsize=(width,height), dpi=dpi)
+   # plt.xticks([100, 300,400, 500, 600], ['A', 'B', 'C'])
+    
+    ticks = np.linspace(0, img_width, 4)
+    x_ticks = np.linspace(xmin, xmax, 4)
+    
+    x_ticks = x_ticks.round(decimals=2)
+    print("TicksNeu {a}".format(a=ticks))
+    print("XTicksNeu {a}".format(a=x_ticks))
+    
+    plt.xticks(ticks, x_ticks)
+
+    norm = colors.PowerNorm(0.3)
+    plt.imshow(z.T,cmap=cmap, origin='lower', norm=norm)
+   # plt.hist(z.T, histtype="stepfilled", bins=100, facecolor="green")
     plt.show()
 
-mandelbrot_image(-2.0, 0.5, -1.25, 1.25, 100,100, 300)
+mandelbrot_image(-2.0, 0.5, -1.25, 1.25, 9,9, 1000, cmap='gnuplot2')
 
 
