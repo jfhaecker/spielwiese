962a81a88ec1f7d70a78c0cd4fd1062c124e2102
jfhaecker <joerghaecker@gmx.net>
2019-10-15 23:07:34 +0200

    Much better evl now ;-)

--- golang/src/evl.go
+++ golang/src/evl.go
@@ -8,45 +8,45 @@ type Node struct {
 	next *Node
 }
 
-func append(root *Node, str string) *Node {
+func evlAppend(root *Node, str string) *Node {
 	if root == nil {
-		root = &Node{str: str, next: nil}
-	} else {
-		curr := root
-		for curr.next != nil {
-			curr = curr.next
-		}
-		curr.next = &Node{str: str, next: nil}
+		return &Node{str: str, next: nil}
 	}
+
+	curr := root
+	for curr.next != nil {
+		curr = curr.next
+	}
+	curr.next = &Node{str: str, next: nil}
 	return root
 }
 
-func prepend(root *Node, str string) *Node {
+func evlPrepend(root *Node, str string) *Node {
 	if root == nil {
-		root = &Node{str: str, next: nil}
-	} else {
-		curr := &Node{str: str, next: nil}
-		curr.next = root
-		root = curr
+		return &Node{str: str, next: nil}
 	}
+
+	curr := &Node{str: str, next: nil}
+	curr.next = root
+	root = curr
 	return root
 }
 
-func insert(root *Node, str string, pos int) (*Node, error) {
+func evlInsert(root *Node, str string, pos int) (*Node, error) {
 	if root == nil {
-		root = &Node{str: str, next: nil}
-	} else {
-		curr := root
-		prev := root
-		for i := 0; i < pos; i++ {
-			prev = curr
-			curr = curr.next
-			if curr == nil {
-				return root, errors.New("Peng krach rummsdibums")
-			}
-		}
-		prev.next = &Node{str: str, next: curr}
+		return &Node{str: str, next: nil}, nil
 	}
+
+	curr := root
+	prev := root
+	for i := 0; i < pos; i++ {
+		prev = curr
+		curr = curr.next
+		if curr == nil {
+			return root, errors.New("Peng krach rummsdibums")
+		}
+	}
+	prev.next = &Node{str: str, next: curr}
 	return root, nil
 }
 
@@ -62,33 +62,53 @@ func printlist(root *Node) {
 	fmt.Println()
 }
 
+func toArray(root *Node) []string {
+	curr := root
+	output := make([]string, 0)
+	for curr != nil {
+		output = append(output, curr.str)
+		curr = curr.next
+	}
+	return output
+}
+
 func main() {
 	str := "Einfach verzeigerte Liste"
 	str2 := " ollaH"
 	var root *Node
 	fmt.Println("-----------Einfaches append")
 	for i := range str {
-		root = append(root, string(str[i]))
+		root = evlAppend(root, string(str[i]))
 	}
 	printlist(root)
+	fmt.Printf("%#v\n", toArray(root))
 
 	fmt.Println("-----------Einfaches prepend")
 	for i := range str2 {
-		root = prepend(root, string(str2[i]))
+		root = evlPrepend(root, string(str2[i]))
 	}
 	printlist(root)
+	fmt.Printf("%#v\n", toArray(root))
 
 	fmt.Println("-----------Einfaches insert")
-	root, err := insert(root, "w", 18)
+	root, err := evlInsert(root, "w", 18)
 	if err == nil {
 		printlist(root)
 	} else {
 		fmt.Println("Error")
 		fmt.Println(err)
 	}
+	fmt.Printf("%#v\n", toArray(root))
 
+	arr := toArray(root)
+	fmt.Println("-------------------")
+	for num, i := range arr {
+		fmt.Printf("%v:%v\n", num, i)
+	}
+
+	fmt.Println("-------------------")
 	fmt.Println("-----------kaputtes insert")
-	root, err = insert(root, "X", 100)
+	root, err = evlInsert(root, "X", 32)
 	if err == nil {
 		printlist(root)
 	} else {
