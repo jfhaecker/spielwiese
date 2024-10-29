8333f58f81e41a856439ba8ecdc7f5a62224bf3a
haex-git <haex-git@moria.local>
2024-10-29 15:57:25 +0100

    damen

--- python/damenwahl/damen.py
+++ python/damenwahl/damen.py
@@ -9,10 +9,13 @@ def dame(reihen, spalten):
     #print(f"Funktion dame Adresse: {inspect.currentframe().f_code}")
     stack.append("o")
     print(f"{''.join(stack)}->dame: {inspect.currentframe().f_locals}")
+    #print(f"{inspect.currentframe().f_back}")
+    #print(f"{inspect.currentframe().f_code.co_name}")
     #print(f"Funktion dame Caller: {inspect.currentframe().f_back}")
    # print(f"{inspect.stack()[0]}")
     if reihen == 0:
-        print(f"{''.join(stack)}->dame fertig")
+        #print(f"{''.join(stack)}->dame fertig")
+        print(f"{''.join(stack)}->dame fertig: {inspect.currentframe().f_locals}")
         return [[]]
     else:
         k = dame(reihen - 1, spalten)
@@ -38,7 +41,7 @@ global stack
 stack= []
 
 print("_________________________")
-k = dame(2,2)
+k = dame(4,4)
 print("_________________________")
 print(f"{len(k)} Lösungen")
 for l in k:
